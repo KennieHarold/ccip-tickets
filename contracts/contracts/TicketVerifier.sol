15449efe97457a8c99510d85d4e1448ebc28a119
kennie <kennie.harold@gmail.com>
2023-11-22 23:30:25 +0800

    Initial commit

---
+++ contracts/contracts/TicketVerifier.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import { PrimitiveTypeUtils } from "@iden3/contracts/lib/PrimitiveTypeUtils.sol";
+import { ICircuitValidator } from "@iden3/contracts/interfaces/ICircuitValidator.sol";
+import { ZKPVerifier } from "@iden3/contracts/verifiers/ZKPVerifier.sol";
+
+contract TicketVerifier is ZKPVerifier {
+	function _beforeProofSubmit(
+		uint64,
+		uint256[] memory inputs,
+		ICircuitValidator validator
+	) internal view override {}
+
+	function _afterProofSubmit(
+		uint64 requestId,
+		uint256[] memory inputs,
+		ICircuitValidator validator
+	) internal override {}
+}
